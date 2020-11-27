b224fac9ce9f5fc8db7141f68c2b3e5de0623dc9
EmiPeralta <emilianop96@hotmail.com>
2020-11-27 18:53:50 -0300

    ApolloProvider component added

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,18 @@
+import App from './App';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:5000',
+    cache: new InMemoryCache(),
+    connectToDevTools: true,
+});
+
+const ApolloProviderComponent = () => {
+    return (
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    );
+};
+
+export default ApolloProviderComponent;
